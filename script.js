1b3339bd66603a02f180399351491ec67b289a24
DanDreev1 <75595828+DanDreev1@users.noreply.github.com>
2024-11-24 16:36:38 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// This code adds a click handler to the element with the class `.hamMenu`. When you click on the hamburger (menu icon), 
+// the `activeham` classes of the hamburger and the menu (`.menu`) are toggled. This allows you to change their style, for example to show or hide the menu on mobile devices.
+
 const hamburger = document.querySelector(".hamMenu");
 const navMenu = document.querySelector(".menu");
 
